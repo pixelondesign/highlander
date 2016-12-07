7d679e9c3a790747de79bcc362d1b52512327723
Ubaid Akbar <ubaid_akbar@yahoo.com>
2016-12-07 20:31:43 +0500

    Update
    New File for Snipet

--- main-tail.js
+++ main-tail.js
@@ -1,19 +1,44 @@
-butt = document.getElementById("target");
-butt.style.fontSize = 30;
-changefont(1);
-
-
-function changefont(add) {
-    butt = document.getElementById("target");
-    //alert(parseInt(butt.style.fontSize));
-    if (parseFloat(butt.style.fontSize) == 30) {
-        add = 1;
-        //butt.style.fontSize = butt.style.fontSize + 1;
+$(document).ready(function () {
+    var names = ["Adil Murad", "Ubaid Akbar","Mr. Jhon","Dsultan","Kabeer"];
+    var places = ["United States", "United Kingdem","London","Paris","Pakistan"];
+    var total = 34;
+    
+    var items = document.getElementsByClassName('item');
+    var count = total;
+    slideup();
+    function slideup() {
+        var itr = randAB(0,(names.length - 1));
+        $("#nmber").text("(" + count + ")");
+        $("#name1").text(names[itr]);
+        $("#address").text(places[itr]);
+        var d = new Date();
+        $("#mytime").text(d.getHours() + ":" + d.getMinutes()+" am");
+        if(d.getHours() > 12){
+            $("#mytime").text((d.getHours()-12) + ":" + d.getMinutes()+" pm");
+        }
+        $('#item1').animate({
+            'marginTop': 10
+        }, 500);
+        $("#item1").removeClass('new');
+        window.setTimeout(function () {
+            $('#item1').animate({
+                'marginTop': -100
+            }, 500);
+            setTimeout(function () {
+                $('#item1').animate({
+                    'marginTop': 120
+                }, 0);
+                count--;
+                if (count < 0) {
+                    count = total;
+                }
+                slideup();
+            }, 500);
+        }, 2000);
     }
-    if (parseFloat(butt.style.fontSize) == 35) {
-        add = -1;
 
-    }
-    butt.style.fontSize = parseFloat(butt.style.fontSize) + add;
-    setTimeout("changefont(" + add + ")", 30);
+});
+function randAB(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
